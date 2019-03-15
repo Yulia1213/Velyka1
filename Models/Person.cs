f0900802feef315a88eb6e9dbca5914e7659bf8c
Yuliia Velyka <yuliaweluka@gmail.com>
2019-03-15 22:14:38 +0200

    Initial commit

---
+++ Models/Person.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Velyka1.Models
+{
+    class Person
+    {
+
+        private DateTime _birthday;
+
+        public Person(DateTime birthday)
+        {
+            Birthday = birthday;
+        }
+
+        public DateTime Birthday
+        {
+            get => _birthday;
+            set
+            {
+                
+                _birthday = value;
+            }
+        }
+
+
+        public int Age()
+        {
+
+            if (DateTime.Now.Month < Birthday.Month
+                || DateTime.Now.Month == Birthday.Month && DateTime.Now.Day < Birthday.Day)
+            {
+                return DateTime.Now.Year - Birthday.Year - 1;
+            }
+
+            return DateTime.Now.Year - Birthday.Year;
+        }
+
+        public string sunSign()
+        {
+            int day = Birthday.Day;
+            int month = Birthday.Month;
+            string sign = "some error";
+
+            if (month == 12)
+            {
+
+                if (day < 22)
+                    sign = "Sagittarius";
+                else
+                    sign = "capricorn";
+            }
+
+            else if (month == 1)
+            {
+                if (day < 20)
+                    sign = "Capricorn";
+                else
+                    sign = "aquarius";
+            }
+
+            else if (month == 2)
+            {
+                if (day < 19)
+                    sign = "Aquarius";
+                else
+                    sign = "pisces";
+            }
+
+            else if (month == 3)
+            {
+                if (day < 21)
+                    sign = "Pisces";
+                else
+                    sign = "aries";
+            }
+            else if (month == 4)
+            {
+                if (day < 20)
+                    sign = "Aries";
+                else
+                    sign = "taurus";
+            }
+
+            else if (month == 5)
+            {
+                if (day < 21)
+                    sign = "Taurus";
+                else
+                    sign = "gemini";
+            }
+
+            else if (month == 6)
+            {
+                if (day < 21)
+                    sign = "Gemini";
+                else
+                    sign = "cancer";
+            }
+
+            else if (month == 7)
+            {
+                if (day < 23)
+                    sign = "Cancer";
+                else
+                    sign = "leo";
+            }
+
+            else if (month == 8)
+            {
+                if (day < 23)
+                    sign = "Leo";
+                else
+                    sign = "virgo";
+            }
+
+            else if (month == 9)
+            {
+                if (day < 23)
+                    sign = "Virgo";
+                else
+                    sign = "libra";
+            }
+
+            else if (month == 10)
+            {
+                if (day < 23)
+                    sign = "Libra";
+                else
+                    sign = "scorpio";
+            }
+
+            else if (month == 11)
+            {
+                if (day < 22)
+                    sign = "scorpio";
+                else
+                    sign = "sagittarius";
+            }
+
+            return sign;
+        }
+        public bool isBirthday()
+        {
+            return DateTime.Now.Year == Birthday.Year && DateTime.Now.Month == Birthday.Month &&
+                   DateTime.Now.Day == Birthday.Day;
+        }
+        public String chineseSign()
+        {
+
+
+            int sign = Birthday.Year % 12;
+
+            switch (sign)
+            {
+                case 0:
+                    return "Monkey year";
+                case 1:
+                    return "Cock year";
+                case 2:
+                    return "Dog year";
+                case 3:
+                    return "Pig year";
+                case 4:
+                    return "Rat year";
+                case 5:
+                    return "Bull year";
+                case 6:
+                    return "Tiger year";
+                case 7:
+                    return "Rabbit year";
+                case 8:
+                    return "Dragon year";
+                case 9:
+                    return "Snake year";
+                case 10:
+                    return "Horse year";
+                case 11:
+                    return "Goat year";
+
+            }
+            return "some error";
+
+
+        }
+
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Velyka1.Models;
+
+namespace Velyka1.ViewModels
+{
+    class MainViewModels : INotifyPropertyChanged
+    {
+
+        private DateTime? _birthday;
+
+
+        private string _sunSign;
+        private string _chineseSign;
+
+        private string _age;
+
+        private string _wishes;
+
+        public DateTime? Birthday
+        {
+            get { return _birthday; }
+            set
+            {
+                DateTime BirthdayTime = value ?? DateTime.Now;
+
+                Person person = new Person(BirthdayTime);
+               
+                if (person.Age() < 135&&person.Age()>0)
+                {
+                    Procced(person);
+                }
+                else
+                {
+                    MessageBox.Show("error");
+                }
+
+                _birthday = value;
+                NotifyPropertyChanged();
+
+
+            }
+        }
+
+        async void InitFullStack(Person p)
+        {
+
+            await Task.Run(() => Age = p.Age().ToString());
+        }
+
+        public async void Procced(Person p)
+        {
+            await Task.Run(() => ProccedMethod(p));
+        }
+
+        void ProccedMethod(Person p)
+        {
+            SunSign = p.sunSign();
+            ChineseSign = p.chineseSign();
+            if (p.isBirthday())
+            {
+                Wishes = "Best wishes";
+            }
+            else
+            {
+
+                Wishes = "";
+
+            }
+
+            Age = p.Age().ToString();
+
+        }
+
+        public string SunSign
+        {
+            get { return _sunSign; }
+            set
+            {
+                _sunSign = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string ChineseSign
+        {
+            get { return _chineseSign; }
+            set
+            {
+                _chineseSign = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Wishes
+        {
+            get { return _wishes; }
+            set
+            {
+                _wishes = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        public string Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged(
+            [CallerMemberName] String propertyName = "")
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
